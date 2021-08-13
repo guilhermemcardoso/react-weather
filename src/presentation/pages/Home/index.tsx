79afb726b05b0ee0a0cbe0fa467df206d4d9a4e8
Guilherme Cardoso <guilherme@Guilhermes-MacBook-Air.local>
2021-08-13 20:27:24 -0300

    feature: created presentation layer related to home page

---
+++ src/presentation/pages/Home/index.tsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+import { GetLocationWeather } from "../../../domain/usecases/get-location-weather";
+import { LocationWeather } from "../../../domain/models/location-weather";
+
+type Props = {
+  getLocationWeather: GetLocationWeather;
+};
+
+const Weather: React.FC<Props> = ({ getLocationWeather }) => {
+  const [weathers, setWeathers] = useState<LocationWeather[]>([]);
+
+  useEffect(() => {}, []);
+
+  async function handleClick(locationId: number) {
+    const response = await getLocationWeather.get({ locationId });
+    setWeathers(response);
+  }
+
+  return (
+    <div>
+      <h2>Weather</h2>
+      <div>
+        <button onClick={() => handleClick(742676)}>Lisboa</button>
+        <button onClick={() => handleClick(44418)}>Londres</button>
+        <button onClick={() => handleClick(2459115)}>Nova Iorque</button>
+        <button onClick={() => handleClick(455827)}>Sao Paulo</button>
+      </div>
+      {weathers.map((weather) => {
+        return (
+          <div key={weather.id}>
+            <small>{weather.applicableDate}</small>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Weather;
